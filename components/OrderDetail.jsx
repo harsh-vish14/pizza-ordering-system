ee937135f68dadc1a16f20abb5b681421ea8950b
harsh-vish14 <harshkv9322@gmail.com>
2022-10-09 11:07:58 +0530

    pushing code to github

---
+++ components/OrderDetail.jsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import styles from "../styles/OrderDetail.module.css";
+
+const OrderDetail = ({ total, createOrder }) => {
+  const [customer, setCustomer] = useState("");
+  const [address, setAddress] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleClick = () => {
+    createOrder({ customer, address, phone, email, total, method: 0 });
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <h1 className={styles.title}>You will pay 20rs after delivery.</h1>
+        <div className={styles.item}>
+          <label className={styles.label}>Name Surname</label>
+          <input
+            placeholder="John Doe"
+            type="text"
+            className={styles.input}
+            onChange={(e) => setCustomer(e.target.value)}
+          />
+        </div>
+        <div className={styles.item}>
+          <label className={styles.label}>Phone Number</label>
+          <input
+            type="text"
+            placeholder="+91 1234 567 890"
+            className={styles.input}
+            onChange={(e) => setPhone(e.target.value)}
+          />
+        </div>
+        <div className={styles.item}>
+          <label className={styles.label}>Email</label>
+          <input
+            type="text"
+            placeholder="abc123@gmail.com"
+            className={styles.input}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className={styles.item}>
+          <label className={styles.label}>Address</label>
+          <textarea
+            rows={5}
+            placeholder="Elton St. 505 NY"
+            type="text"
+            className={styles.textarea}
+            onChange={(e) => setAddress(e.target.value)}
+          />
+        </div>
+        <button className={styles.button} onClick={handleClick}>
+          Order
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default OrderDetail;
